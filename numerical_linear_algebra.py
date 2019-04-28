99c274d095e518c9edb30c6dd348aa3452f5981f
Tim <kindert1@tcnj.edu>
2019-04-28 11:35:23 -0400

    Refactored various methods to account for LU factorization

--- numerical_linear_algebra.py
+++ numerical_linear_algebra.py
@@ -7,16 +7,22 @@ def ref(A,b):
         b = np.array(b)
         
     aug = np.ndarray.astype(np.c_[A,b],float)
+    L = np.identity(A.shape[0])
     for row in range(1,aug.shape[0]):
         for col in range(0,row):
-            aug[row] = aug[row] -(aug[row,col]/aug[col,col])*aug[col]
+            L[row,col] = (aug[row,col]/aug[col,col])
+            aug[row] = aug[row] -L[row,col]*aug[col]
             
-    return aug
+    U = aug[:,:-1]
+    y = aug[:,-1]
+    return L, U, y
 
 
-def back_sub(ref_matrix):
-    m = ref_matrix.shape[0]
+def back_sub(U, y):
+    m = U.shape[0]
     x = np.empty([1,m])
+    ref_matrix = np.c_[U,y]
+    
     for row in reversed(range(m)):
         signs = -np.ones([1,m])
         signs[0,row] = -signs[0,row]
@@ -31,11 +37,32 @@ def back_sub(ref_matrix):
     return x
 
 
+def forward_sub(L,b):
+    m = L.shape[0]
+    d = np.empty([1,m])
+    ref_matrix = np.c_[L,b]
+    
+    
+    
+    for row in range(m):
+        signs = -np.ones([1,m])
+        signs[0,row] = -signs[0,row]
+        ref_matrix[row,:-1] = signs*ref_matrix[row,:-1]
+        
+        temp_sum = 0
+        for i in range(row+1,m):
+            temp_sum += ref_matrix[row,i]*d[0,i]
+            
+        d[0,row] = (temp_sum + ref_matrix[row,-1])/ref_matrix[row,row]
+        
+    return d
+
+
 def gaussian_elimination(A,b):
     assert(np.linalg.det(A) != 0)
     
-    row_echelon_matrix = ref(A,b)
-    x = back_sub(row_echelon_matrix)
+    _, U, y = ref(A,b)
+    x = back_sub(U, y)
     
     return x
 
@@ -64,4 +91,6 @@ def gauss_jordan(A,b=None):
     else:
         return aug[:,-1]
     
-                
+
+def lu():
+    pass            
